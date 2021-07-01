f3b40e23a046f89aac31658c21ae889f57e77346
guiweber <guillaume.web@gmail.com>
2021-07-01 10:15:20 -0400

    Update cfg.sh

--- kiki/cfg.sh
+++ kiki/cfg.sh
@@ -8,3 +8,11 @@ usermod -s /usr/sbin/nologin qbtuser
 dnf update -qy
 dnf install qbittorrent-nox nmap -qy
 
+# creates the service
+cd /etc/systemd/system/
+wget https://raw.githubusercontent.com/guiweber/config/master/kiki/qbittorrent.service
+systemctl daemon-reload
+systemctl start qbittorrent
+systemctl enable qbittorrent
+
+# disable logging to disk
