53b92fbcc1025dd4a1a1e15620360ee67e541c2e
guiweber <guillaume.web@gmail.com>
2021-07-02 04:23:06 -0400

    Update cfg.sh

--- kiki/cfg.sh
+++ kiki/cfg.sh
@@ -17,9 +17,12 @@ systemctl enable qbittorrent
 
 # Port 80 cannot be used by normal users, bu we can redirect it to our port so that we dont have to enter the port in the address bar
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 2000
+# To remove the redirect, the following line can be used
+# iptables -t nat --line-numbers -n -L
 
-# Add firewall rule for our port
+# Add firewall rule for our port and port 80 (both will be useable)
 firewall-cmd --add-port=2000/tcp --permanent
+firewall-cmd --permanent --add-service=http
 systemctl restart firewalld
 
 # disable logging to disk
