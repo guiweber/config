2eea50186beb3ff399078b91b7e52ac6a8493e2a
guiweber <guillaume.web@gmail.com>
2022-04-30 15:19:40 -0400

    Update cfg.sh

--- kiki/cfg.sh
+++ kiki/cfg.sh
@@ -15,14 +15,13 @@ systemctl daemon-reload
 systemctl start qbittorrent
 systemctl enable qbittorrent
 
-# Port 80 cannot be used by normal users, bu we can redirect it to our port so that we dont have to enter the port in the address bar
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 2000
-# To remove the redirect, the following line can be used
-# iptables -t nat --line-numbers -n -L
-
 # Add firewall rule for our port and port 80 (both will be useable)
 firewall-cmd --add-port=2000/tcp --permanent
+### Port 80 cannot be used by normal users (reserved for http), bu we can redirect it to our port so that we dont have to enter the port in the address bar
+firewall-cmd --permanent --add-forward-port=port=80:proto=tcp:toport=2000
 firewall-cmd --permanent --add-service=http
+# If it doesn't work with only this, try this:
+# firewall-cmd --permanent --zone=public --remove-port=80/tcp
 systemctl restart firewalld
 
 # Configure OpenVPN
