09e8c48abdf392e727b6c1d2fa08d389b3abc4f9
guiweber <guillaume.web@gmail.com>
2022-05-28 12:02:29 -0400

    Update cfg.sh

--- kiki/cfg.sh
+++ kiki/cfg.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Create qbittorrent user and disable ssh login
-adduser qbtuser
-usermod -s /usr/sbin/nologin qbtuser
+adduser -s /usr/sbin/nologin qbtuser
 
 # Update and install software
 dnf update -qy
@@ -22,12 +21,16 @@ firewall-cmd --permanent --add-forward-port=port=80:proto=tcp:toport=2000
 firewall-cmd --permanent --add-service=http
 # If it doesn't work with only this, try this:
 # firewall-cmd --permanent --zone=public --remove-port=80/tcp
-sudo firewall-cmd --permanent --add-service=samba
 
 # disable logging to disk
 
 # create shared folder
 mkdir /mnt/usb_drive
+
+# Configure samba
+firewall-cmd --permanent --add-service=samba
+# Add Samba user that cannot login (shell points to /sbin/nologin)
+adduser --no-create-home --shell /sbin/nologin --home /mnt/usb_drive kiki_share
 
 # restart services
 systemctl restart firewalld
