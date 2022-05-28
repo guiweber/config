6a102dddbd6440d8e996e0132b753767532ab564
guiweber <guillaume.web@gmail.com>
2022-05-28 12:18:06 -0400

    Update cfg.sh

--- kiki/cfg.sh
+++ kiki/cfg.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# Create qbittorrent user and disable ssh login
-adduser -s /usr/sbin/nologin qbtuser
-
 # Update and install software
 dnf update -qy
 dnf install qbittorrent-nox nmap wireguard-tools -qy
 
-# creates the service
-cd /etc/systemd/system/
-wget https://raw.githubusercontent.com/guiweber/config/master/kiki/qbittorrent.service
-systemctl daemon-reload
-systemctl start qbittorrent
-systemctl enable qbittorrent
-
-# Add firewall rule for our port and port 80 (both will be useable)
+# Add firewall rule for our WebUI port and port 80 (both will be useable)
 firewall-cmd --add-port=2000/tcp --permanent
 ### Port 80 cannot be used by normal users (reserved for http), bu we can redirect it to our port so that we dont have to enter the port in the address bar
 firewall-cmd --permanent --add-forward-port=port=80:proto=tcp:toport=2000
@@ -32,6 +22,15 @@ firewall-cmd --permanent --add-service=samba
 # Add Samba user that cannot login (shell points to /sbin/nologin)
 adduser --no-create-home --shell /sbin/nologin --home /mnt/usb_drive kiki_share
 
+# Create qbittorrent user and disable ssh login
+adduser -s /usr/sbin/nologin qbtuser
+
+# creates the qbittorrent service
+cd /etc/systemd/system/
+wget https://raw.githubusercontent.com/guiweber/config/master/kiki/qbittorrent.service
+systemctl daemon-reload
+systemctl start qbittorrent
+systemctl enable qbittorrent
+
 # restart services
 systemctl restart firewalld
-systemctl restart qbittorrent
